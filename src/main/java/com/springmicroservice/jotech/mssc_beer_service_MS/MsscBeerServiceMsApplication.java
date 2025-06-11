9738b4c9badee4ea9282022197c770652554f706
yonatanmengesha <yonatan.mengesha2017@gmail.com>
2025-06-11 16:53:51 -0400

    Initial project add. closes #1

---
+++ src/main/java/com/springmicroservice/jotech/mssc_beer_service_MS/MsscBeerServiceMsApplication.java
@@ -0,0 +1,13 @@
+package com.springmicroservice.jotech.mssc_beer_service_MS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBeerServiceMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBeerServiceMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springmicroservice/jotech/mssc_beer_service_MS/MsscBeerServiceMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmicroservice.jotech.mssc_beer_service_MS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
