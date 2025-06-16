9be3126b40d2873b600df4e24c67a47746355414
yonatanmengesha <yonatan.mengesha2017@gmail.com>
2025-06-16 11:46:39 -0400

    adding bootstrap.closes #7

--- src/main/java/com/springmicroservice/jotech/mssc_beer_service_MS/repositories/BeerRepository.java
+++ src/main/java/com/springmicroservice/jotech/mssc_beer_service_MS/repositories/BeerRepository.java
@@ -1,9 +1,10 @@
 package com.springmicroservice.jotech.mssc_beer_service_MS.repositories;
 
 import com.springmicroservice.jotech.mssc_beer_service_MS.domain.Beer;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.UUID;
 
-public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+public interface BeerRepository extends CrudRepository<Beer, UUID> {
 }
