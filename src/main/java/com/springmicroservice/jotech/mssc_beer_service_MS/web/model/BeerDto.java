44068ddbd61c0cd275894e29ba6acbe662cd24ff
yonatanmengesha <yonatan.mengesha2017@gmail.com>
2025-06-16 17:24:08 -0400

    Added bean validation. closes #8

--- src/main/java/com/springmicroservice/jotech/mssc_beer_service_MS/web/model/BeerDto.java
+++ src/main/java/com/springmicroservice/jotech/mssc_beer_service_MS/web/model/BeerDto.java
@@ -1,5 +1,9 @@
 package com.springmicroservice.jotech.mssc_beer_service_MS.web.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,18 +20,28 @@ import java.util.UUID;
 public class BeerDto {
 
 
+    @Null
     private UUID id;
+    @Null
     private Integer version;
 
+    @Null
     private OffsetDateTime createdDate;
+    @Null
     private OffsetDateTime lastModifiedDate;
 
+    @NotBlank
     private String beerName;
 
+    @NotNull
     private BeerStyleEnum beerStyle;
 
+    @NotNull
+    @Positive
     private Long upc;
 
+    @NotNull
+    @Positive
     private BigDecimal price;
 
     private Integer quantityOnHand;
